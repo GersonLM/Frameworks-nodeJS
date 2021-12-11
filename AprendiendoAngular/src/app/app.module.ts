108a50e95496bc0b7ce89f3b8974d653f8dfebd9
GersonLM <lopezmenjivar99@gmail.com>
2021-12-10 22:45:41 -0600

    Sistema de Rutas SPA

--- AprendiendoAngular/src/app/app.module.ts
+++ AprendiendoAngular/src/app/app.module.ts
@@ -1,22 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component'; // se utiliza el nombre de la clase para importarlo
 import { MiComponente } from './components/mi-componente/mi-componente.component';
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
 import { PruebasComponent } from './components/pruebas/pruebas.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [ //cargamos los modulos importados
     AppComponent,
     MiComponente,
     PeliculasComponent,
-    PruebasComponent
+    PruebasComponent,
+    HeaderComponent,
+    SliderComponent,
+    SidebarComponent,
+    FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    FormularioComponent,
+    PaginaComponent,
+    ErrorComponent
   ],
   imports: [  // cargamos los ,odulos de la apliacaion
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [], // cargamos los servicios
+  providers: [appRoutingProviders], // cargamos los servicios
   bootstrap: [AppComponent] // indicamos el componenete inicial, sobre el cual se cargara la aplicaicon
 })
 export class AppModule { }
